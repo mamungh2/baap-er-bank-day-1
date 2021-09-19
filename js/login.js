ae382211299c1be63ad0b3dcdc2f7928643b76db
Md. Rafiqul Islam Mamun <mamun8475@gmail.com>
2021-09-19 11:57:16 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    if (userEmail == 'sontan@baap.com' && userPassword == 'secret') {
+        // check email and password
+        window.location.href = ('banking.html');
+    }
+});
+
